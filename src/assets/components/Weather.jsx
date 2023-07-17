22c3649094682846a269cc1d03a3bea1275f90f1
olygood <olygood@outlook.com>
2023-07-17 13:04:25 +0200

    position elements and border probleme

--- src/assets/components/Weather.jsx
+++ src/assets/components/Weather.jsx
@@ -2,19 +2,22 @@ import React from "react";
 import { useState, useEffect } from "react";
 import "./Weather.css";
 import imgLoader from "../img/loader.svg";
+import icons from "../img/icons/03n.svg";
 
 const Weather = () => {
   const [weather, setWeather] = useState(null);
   return (
     <div className="container-weather">
       <h1>Weather</h1>
-      <p>blabla</p>
-      <p>blabla</p>
-
-      <div className="container-img-loader">
+      <p className="city-name">Liege</p>
+      <p className="country-name">Belgium</p>
+      <p className="temp">0</p>
+      <div className="container-icons">
+        <img src={icons} alt="icon" className="icons-temp" />
+      </div>
+      <div className="container-img-loader ">
         <img src={imgLoader} alt="img" className="img-loader" />
       </div>
-      <p>temps-img</p>
     </div>
   );
 };
